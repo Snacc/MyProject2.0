ff293dd8bee284468c0c2acc235b85edcb32fac8
Snacc <88640@farnborough.ac.uk>
2019-11-08 14:26:04 +0000

    Added Admin Table

--- src/main/java/Controllers/Admin.java
+++ src/main/java/Controllers/Admin.java
@@ -1,6 +1,6 @@
 package Controllers;
-import Server.Main;
 
+import Server.Main;
 import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -10,25 +10,23 @@ import javax.ws.rs.core.MediaType;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-@Path("users")
-public class UserController {
-    // This method lists users from the database, including their firstname, surname, etc. For test purposes the password is not encrypted, or censored in anyway.
+@Path("admin")
+public class Admin {
+    // This method lists all the admin records from the admin table on the database.
     @GET
     @Path("list")
     @Produces(MediaType.APPLICATION_JSON)
-    public String ListUsers(){
-        System.out.println("users/list");
+    public String ListAdmins(){
+        System.out.println("admin/list");
         JSONArray list = new JSONArray();
         try{
-            PreparedStatement ps = Main.db.prepareStatement("SELECT UserID, Username, Password, FirstName, Surname FROM UserDetails");
+            PreparedStatement ps = Main.db.prepareStatement("SELECT AdminID, Username, Password FROM Admin");
             ResultSet results = ps.executeQuery();
             while(results.next()){    //Method keeps getting data until it reaches the end of the database.
                 JSONObject item = new JSONObject();
-                item.put("UserID", results.getInt(1));
+                item.put("AdminID", results.getInt(1));
                 item.put("Username", results.getString(2));
                 item.put("Password", results.getString(3));
-                item.put("FirstName", results.getString(4));
-                item.put("Surname", results.getString(5));
                 list.add(item);
             }
             return list.toString();
@@ -40,25 +38,23 @@ public class UserController {
 
     // Gets a single item from the table
     @GET
-    @Path("get/{UserID}")
+    @Path("get/{AdminID}")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getThing(@PathParam("UserID") Integer UserID) throws Exception {
-        if(UserID==null){
+    public String getThing(@PathParam("AdminID") Integer AdminID) throws Exception {
+        if(AdminID==null){
             throw new Exception("Thing's 'id' is missing in the HTTP request's URL.");
 
         }
-        System.out.println("users/get/" + UserID);
+        System.out.println("admin/get/" + AdminID);
         JSONObject item = new JSONObject();
         try{
-            PreparedStatement ps = Main.db.prepareStatement("Select Username, Password, FirstName, Surname FROM UserDetails WHERE UserID =?");
-            ps.setInt(1,UserID);
+            PreparedStatement ps = Main.db.prepareStatement("Select Username, Password FROM Admin WHERE AdminID =?");
+            ps.setInt(1,AdminID);
             ResultSet results = ps.executeQuery();
             if (results.next()){
-                item.put("UserID", UserID);
+                item.put("AdminID", AdminID);
                 item.put("Username", results.getString(1));
-                item.put("Password",results.getString(2));
-                item.put("FirstName", results.getString(3));
-                item.put("Surname", results.getString(4));
+                item.put("Password", results.getString(2));
             }
             return item.toString();
 
@@ -68,25 +64,23 @@ public class UserController {
         }
     }
 
-    // This method inserts a new user record into the user details table
+    // This method inserts a new admin record into the admin table
 
     @POST
     @Path("new")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Produces(MediaType.APPLICATION_JSON)
-    public String InsertIntoUsers(@FormDataParam("UserID") Integer UserID, @FormDataParam("Username") String Username, @FormDataParam("Password") String Password, @FormDataParam("FirstName") String FirstName, @FormDataParam("Surname") String Surname){
+    public String InsertIntoAdmin(@FormDataParam("AdminID") Integer AdminID, @FormDataParam("Username") String Username, @FormDataParam("Password") String Password){
         try{
-            if (UserID == null || Username == null || Password == null || FirstName == null || Surname == null){
+            if (AdminID == null || Username == null || Password == null){
                 throw new Exception("One or more form data parameters are missing in the HTTP request.");
             }
-            System.out.println("users/newid=" +UserID);
-            PreparedStatement ps = Main.db.prepareStatement("INSERT INTO UserDetails(UserID, Username, Password, FirstName, Surname) VALUES (?,?,?,?,?)");
+            System.out.println("admin/newid=" +AdminID);
+            PreparedStatement ps = Main.db.prepareStatement("INSERT INTO Admin(AdminID, Username, Password) VALUES (?,?,?)");
 
-            ps.setInt(1, UserID);
+            ps.setInt(1, AdminID);
             ps.setString(2, Username);
             ps.setString(3, Password);
-            ps.setString(4, FirstName);
-            ps.setString(5, Surname);
             ps.execute();
             return "{\"status\": \"OK\"}";
 
@@ -102,18 +96,16 @@ public class UserController {
     @Path("update")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Produces(MediaType.APPLICATION_JSON)
-    public String UpdateUserDetails(@FormDataParam("UserID") Integer UserID, @FormDataParam("Username") String Username, @FormDataParam("Password") String Password, @FormDataParam("FirstName") String FirstName, @FormDataParam("Surname") String Surname){
+    public String UpdateAdmin(@FormDataParam("AdminID") Integer AdminID, @FormDataParam("Username") String Username, @FormDataParam("Password") String Password){
         try{
-            if (UserID == null || Username == null || Password == null || FirstName == null || Surname == null){
+            if (AdminID == null || Username == null || Password == null){
                 throw new Exception("One or more form data parameters are missing in the HTTP request.");
             }
-            System.out.println("users/update UserID=" + UserID);
-            PreparedStatement ps = Main.db.prepareStatement("UPDATE UserDetails SET Username = ?, Password = ?, Firstname = ?, Surname = ? WHERE UserID = ?");
+            System.out.println("admin/update AdminID=" + AdminID);
+            PreparedStatement ps = Main.db.prepareStatement("UPDATE Admin SET Username = ?, Password = ? WHERE AdminID = ?");
             ps.setString(1,Username);
             ps.setString(2,Password);
-            ps.setString(3,FirstName);
-            ps.setString(4,Surname);
-            ps.setInt(5,UserID);
+            ps.setInt(3,AdminID);
             ps.execute();
             return "{\"status\": \"OK\"}";
 
@@ -124,19 +116,19 @@ public class UserController {
         }
     }
 
-    // This method lets you delete any records from the database, by UserID
+    // This method lets you delete any records from the database, by AdminID
     @POST
     @Path("delete")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Produces(MediaType.APPLICATION_JSON)
-    public String DeleteUserDetails(@FormDataParam("UserID") Integer UserID){
+    public String DeleteAdmin(@FormDataParam("AdminID") Integer AdminID){
         try{
-            if(UserID==null){
+            if(AdminID==null){
                 throw new Exception("One or more form data parameters are missing in the HTTP request.");
             }
-            System.out.println("users/delete UserID=" + UserID);
-            PreparedStatement ps = Main.db.prepareStatement("DELETE FROM UserDetails WHERE UserID = ?");
-            ps.setInt(1,UserID);
+            System.out.println("admin/delete AdminID=" + AdminID);
+            PreparedStatement ps = Main.db.prepareStatement("DELETE FROM Admin WHERE AdminID = ?");
+            ps.setInt(1,AdminID);
             ps.execute();
             return"{\"status\": \"OK\"}";
 
@@ -146,21 +138,4 @@ public class UserController {
             return "{\"error\": \"Unable to delete item, please see server console for more info.\"}";
         }
     }
-
-    /*
-    @POST
-    @Path("login")
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    @Produces(MediaType.APPLICATION_JSON)
-    public String Login(@FormDataParam("Username") String Username, @FormDataParam("Password") String Password){
-        try{
-            if(Username == null || Password == null){
-                throw new Exception("One or more login parameters are missing");
-            }
-        }
-
-    }
-    */
-
-
 }
