9e961424f1e6e231893a2c113edd2e5e25e71c22
Snacc <88640@farnborough.ac.uk>
2019-12-17 10:28:19 +0000

    Fixed my valid token issue, added a User.java class for login/logout. NEED TO CREATE A REGISTER FUNCTION ASAP (do at home please michael i beg)

--- src/main/java/Controllers/UserController.java
+++ src/main/java/Controllers/UserController.java
@@ -15,6 +15,7 @@ import java.util.UUID;
 
 @Path("users")
 public class UserController {
+
     // This method lists users from the database, including their firstname, surname, etc. For test purposes the password is not encrypted, or censored in anyway.
     @GET
     @Path("list")
@@ -78,9 +79,6 @@ public class UserController {
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Produces(MediaType.APPLICATION_JSON)
     public String InsertIntoUsers(@FormDataParam("UserID") Integer UserID, @FormDataParam("Username") String Username, @FormDataParam("Password") String Password, @FormDataParam("FirstName") String FirstName, @FormDataParam("Surname") String Surname, @CookieParam("Token") String Token){
-        if(!UserID.validToken(Token)){
-            return "{\"error\": \"You don't appear to be logged in\"}";
-        }
         try{
             if (UserID == null || Username == null || Password == null || FirstName == null || Surname == null){ //If inputs are null, throws exeception.
                 throw new Exception("One or more form data parameters are missing in the HTTP request.");
@@ -152,77 +150,6 @@ public class UserController {
             return "{\"error\": \"Unable to delete item, please see server console for more info.\"}";
         }
     }
-
-    //login method - produces a token for logged in user
-    @POST
-    @Path("login")
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    @Produces(MediaType.APPLICATION_JSON)
-    public String userLogin(@FormDataParam("Username") String Username,@FormDataParam("Password") String Password, @CookieParam("Token") String Token){
-        try{
-            PreparedStatement ps = Main.db.prepareStatement("SELECT Password FROM Users WHERE Username = ?");
-            ps.setString(1, Username);
-            ResultSet loginResults = ps.executeQuery();
-            if(loginResults.next()){
-                String correctPassword=loginResults.getString(1);//verifies password is correct on database
-                if(Password.equals(correctPassword)){
-                    Token = UUID.randomUUID().toString();
-                    PreparedStatement ps2 = Main.db.prepareStatement("UPDATE Users SET Token = ? WHERE Username = ?");
-                    ps2.setString(1, Token);
-                    ps2.setString(2, Username);
-                    ps2.executeUpdate();
-
-                    return "{\"Token\": \"" + Token + "\"}"; //returns the generated token
-
-
-
-
-                } else {
-                    return "{\"error\": \"Incorrect password!\"}"; //if password is incorrect
-                }
-            } else{
-                return "{\"error\": \"Incorrect password!\"}"; //if password is incorrect
-
-            }
-        } catch (Exception exception){
-            System.out.println("Database error during /user/login: " + exception.getMessage());
-            return "{\"error\": \"Server side error :(\"}";
-        }
-    }
-
-    //logout method
-    @POST
-    @Path("logout")
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    @Produces(MediaType.APPLICATION_JSON)
-    public String userLogout(@CookieParam("Token") String Token){ //takes corresponding user's token
-        try{
-            System.out.println("/users/logout");
-            PreparedStatement ps = Main.db.prepareStatement("SELECT UserID FROM Users WHERE Token =?");
-            ps.setString(1,Token);
-            ResultSet logoutResults = ps.executeQuery();
-            if (logoutResults.next()){
-                int ID = logoutResults.getInt(1);
-                PreparedStatement ps2 = Main.db.prepareStatement("UPDATE Users SET Token = NULL Where UserID = ?");
-                ps2.setInt(1,ID);
-                ps2.executeUpdate();
-                return "{\"status\": \"OK\"}";
-
-            } else{
-                return "{\"error\": \"Invalid token\"}";
-            }
-
-        } catch (Exception exception){
-            System.out.println("Database error during /users/logout - could not update" +exception.getMessage()); //prints error in console
-            return "{\"error\": \"Server side error :(\"}"; //prints error for user
-        }
-
-    }
-
-
-
-
-
 
 
 }
