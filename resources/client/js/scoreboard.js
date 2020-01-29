cb5140b55b605aa000100b1a746fb1e04e6da0e2
88640 <88640@farnborough.ac.uk>
2020-01-29 12:55:29 +0000

    Added some comments

--- resources/client/js/scoreboard.js
+++
@@ -1,4 +0,0 @@
-function pageLoad(){
-    let LeaderboardHTML = <table-align="center"> +
-
-}
--- src/main/java/Controllers/User.java
+++ src/main/java/Controllers/User.java
@@ -18,17 +18,17 @@ public class User {
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Produces(MediaType.APPLICATION_JSON)
     // Takes Username and Password as a login
-    public String userLogin(@FormDataParam("Username") String Username, @FormDataParam("Password") String Password){
+    public String userLogin(@FormDataParam("Username") String Username, @FormDataParam("Password") String Password){ //takes parameters
         try{
             System.out.println("user/login");
             PreparedStatement ps1= Main.db.prepareStatement("SELECT Password FROM Users WHERE Username = ?");
             ps1.setString(1, Username);
             ResultSet loginResults = ps1.executeQuery();
-            if (loginResults.next()){
+            if (loginResults.next()){ //if there is an identifiable input then correctPassword is defined
                 String correctPassword = loginResults.getString(1);
                 if (Password.equals(correctPassword)){
-                    String Token = UUID.randomUUID().toString();
-                    PreparedStatement ps2 = Main.db.prepareStatement("UPDATE Users SET Token = ? WHERE Username = ?");
+                    String Token = UUID.randomUUID().toString(); //random token generated
+                    PreparedStatement ps2 = Main.db.prepareStatement("UPDATE Users SET Token = ? WHERE Username = ?"); //token set
                     ps2.setString(1,Token);
                     ps2.setString(2,Username);
                     ps2.executeUpdate();
@@ -40,19 +40,20 @@ public class User {
 
 
                 } else{
-                    return "{\"error\": Incorrect Password!\"}";
+                    return "{\"error\": Incorrect Password!\"}"; //check HTML input if invalid
                 }
 
             }else {
-                return "{\"error\": \"Unknown User!\"}";
+                return "{\"error\": \"Unknown User!\"}"; //check HTML inputs if this comes up.
             }
         } catch (Exception exception){
             System.out.println("Database error during /user/login: " +exception.getMessage());
-            return "{\"error\": \"Server side error!\"}";
+            return "{\"error\": \"Server side error!\"}"; //check HTML input/api implementation if this shows
         }
     }
 
 
+    //logout API
     @POST
     @Path("logout")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
@@ -60,25 +61,27 @@ public class User {
     public String userLogout(@CookieParam("Token") String Token) {//takes corresponding user's token
         try {
             System.out.println("user/logout");
-            PreparedStatement ps1 = Main.db.prepareStatement("SELECT UserID FROM Users WHERE Token = ?");
+            PreparedStatement ps1 = Main.db.prepareStatement("SELECT UserID FROM Users WHERE Token = ?"); //ID determined by token input
             ps1.setString(1, Token);
             ResultSet logoutResults = ps1.executeQuery();
-            if (logoutResults.next()) {
+            if (logoutResults.next()) { //if there is an identifiable token which matches, then the record shall be amended
                 int id = logoutResults.getInt(1);
-                PreparedStatement ps2 = Main.db.prepareStatement("UPDATE Users SET Token = NULL WHERE UserID = ?");
+                PreparedStatement ps2 = Main.db.prepareStatement("UPDATE Users SET Token = NULL WHERE UserID = ?"); //sets token to null
                 ps2.setInt(1, id);
                 ps2.executeUpdate();
 
-                return "{\"status\": \"OK\"}";
+                return "{\"status\": \"OK\"}"; //function carried out
             } else {
-                return "{\"error\": \"Invalid token!\"}";
+                return "{\"error\": \"Invalid token!\"}"; //Check HTML input if this is invalid
             }
         } catch (Exception exception) {
             System.out.println("Database error during /user/logout: " + exception.getMessage());
-            return "{\"error\": \"Server side error!\"}";
+            return "{\"error\": \"Server side error!\"}"; //Check HTML input if this error pops up
         }
     }
 
+
+    //if this function is ever run, check token in browser console/window, otherwise something has gone wrong with the login API.
     public static boolean validToken(String Token){
         try{
             PreparedStatement ps = Main.db.prepareStatement("SELECT UserID FROM Users WHERE Token = ?");
@@ -92,6 +95,8 @@ public class User {
         }
     }
 
+
+    //register API
     @POST
     @Path("register")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
@@ -104,6 +109,7 @@ public class User {
             PreparedStatement ps = Main.db.prepareStatement("INSERT INTO Users(Username, Password, FirstName, Surname) VALUES (?,?,?,?)");
             //sets parameter data types
 
+            //UserID does not need to be inputted as the database auto increments
             ps.setString(1, Username);
             ps.setString(2, Password);
             ps.setString(3, FirstName);
